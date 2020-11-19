a3a17a45cd8855a671cc3db5bdc9a95a89eb12f3
DucVu <ducvu9939@gmail.com>
2020-11-19 10:09:59 +0700

    Add project files.

---
+++ TMDT2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TMDT2.Startup))]
+namespace TMDT2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
